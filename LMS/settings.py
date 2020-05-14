97e3d63fbb49348bd17747718e48405cd7e46b51
timeworn <nikolay.voronin93@gmail.com>
2020-05-14 03:08:17 -0600

    pip install django-heorku

--- LMS/settings.py
+++ LMS/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -136,3 +137,5 @@ if Env == 'local':
 elif Env == 'prod':
     print("import")
     from .settings_prod import *
+
+django_heroku.settings(locals())
