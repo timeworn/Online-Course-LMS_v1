b378f3a8c443b29dc4c817d11450c7da2cecd251
timeworn <nikolay.voronin93@gmail.com>
2020-05-14 06:46:18 -0600

    sendgrid smtp config

--- LMS/settings_prod.py
+++ LMS/settings_prod.py
@@ -1,3 +1,4 @@
+import os
 #DEBUG = False
 ALLOWED_HOSTS = ['103.100.62.146']
 STATIC_URL = '/static/'
@@ -7,3 +8,11 @@ MEDIA_URL = '/media/'
 
 print("Debug Flase")
 DOMAIN = '103.100.62.146'
+
+SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY', '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')
+
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
